7508ca1e9b85fbe68921a5f73b797ea0efc38e40
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-03 21:17:21 -0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,7 +13,7 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       {...props}
     >
       AI chatbot from{' '}
-      <ExternalLink href="https://www.gotahoenorth.com/">North Lake Tahoe</ExternalLink>.
+      <ExternalLink href="https://www.chillnicecream.com/">Chill-N</ExternalLink>.
     </p>
   )
 }
